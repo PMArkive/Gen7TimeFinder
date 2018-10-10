921fb38f7b88de60b97c5c9f97fac0c6607089ff
Admiral-Fish <admiralfish420@gmail.com>
2018-10-10 15:51:36 -0700

    Style test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QStyleFactory>
 #include <Forms/MainWindow.hpp>
 
 int main(int argc, char *argv[])
@@ -6,6 +7,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName("Gen7TimeFinder");
     a.setOrganizationName("Admiral-Fish");
+
+#ifdef Q_OS_WIN
+    a.setStyle(QStyleFactory::create("windowsvista"));
+#endif
 
     MainWindow w;
     w.show();
